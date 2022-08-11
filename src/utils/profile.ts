6cae888d37f220cef270677d241db1e0e374aa35
Jazim Abbas <jazimabbas87@gmail.com>
2022-08-11 17:44:22 +0500

    added typescript

---
+++ src/utils/profile.ts
@@ -0,0 +1,65 @@
+import os from "os";
+import fs from "fs/promises";
+import { AWSProfile } from "./types";
+
+export class AwsProfile {
+  private awsDir: string;
+  private profiles: any;
+
+  constructor(private profile: AWSProfile) {
+    this.awsDir = "";
+    this.profiles = {};
+  }
+
+  async createProfile() {
+    await this.createAWSDirIfNotExists();
+    await this.readProfilesFromDisk();
+
+    if (this.isProfileExists(this.profile.profileName)) {
+      return;
+    }
+
+    await this.createCredentialsProfile();
+  }
+
+  private async createAWSDirIfNotExists(): Promise<void> {
+    this.awsDir = os.homedir() + "/.aws";
+
+    try {
+      await fs.mkdir(this.awsDir);
+    } catch (_) {}
+  }
+
+  private async readProfilesFromDisk(): Promise<void> {
+    const filePath = this.awsDir + "/profiles.json";
+
+    try {
+      const content = await fs.readFile(filePath);
+      this.profiles = JSON.parse(content.toString());
+    } catch (_) {
+      this.profiles = {};
+    }
+  }
+
+  private isProfileExists(profileName: string): boolean {
+    if (this.profiles[profileName]) return true;
+    return false;
+  }
+
+  private async createCredentialsProfile() {
+    const { profileName, awsAccessKey, awsSecretKey } = this.profile;
+
+    const credentialsFilePath = this.awsDir + "/credentials";
+    const profileContent = `
+        [${profileName}]
+        aws_access_key_id=${awsAccessKey}
+        aws_secret_access_key=${awsSecretKey}
+    `;
+
+    try {
+      await fs.appendFile(credentialsFilePath, profileContent);
+    } catch (err) {
+      throw err;
+    }
+  }
+}
